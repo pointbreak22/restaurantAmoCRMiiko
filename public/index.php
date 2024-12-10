bdedc33679f33e7f3272093b6b3c07e3fd86bcef
POINTBREAK22 <zbrozhek13@gmail.com>
2024-12-10 12:04:22 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+define('APP_PATH', dirname(__DIR__));
+require_once APP_PATH . '/vendor/autoload.php';
+
+use App\Kernel\App;
+use Symfony\Component\Dotenv\Dotenv;
+
+try {
+    //dd($_SERVER);
+    $dotenv = new Dotenv();
+    $dotenv->loadEnv(APP_PATH . '/.env');
+
+    define("APP_ENV", $_SERVER['APP_ENV']);
+    define("APP_URL", $_SERVER['APP_URL']);
+
+
+
+    $app = new App();
+    $app->run();
+} catch (Throwable $th) {
+    header("Content-Type: application/json");
+    echo json_encode([
+        'code' => $th->getCode(),
+        'message' => $th->getMessage(),
+        'file' => $th->getFile(),
+        'line' => $th->getLine(),
+    ], JSON_PRETTY_PRINT);
+}
